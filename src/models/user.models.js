a7226466e78950da7b3fc1dc8c55452e85baffd2
Jair <unifly.jair@gmail.com>
2025-02-12 12:15:34 -0500

    feature/ user model creation

---
+++ src/models/user.models.js
@@ -0,0 +1,46 @@
+import { DataTypes } from "sequelize";
+
+const modelName = "User";
+
+const User = sequelize.define(modelName, {
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  lastname: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM("admin", "user"),
+    allowNull: false,
+    defaultvalue: "user",
+  },
+});
+
+await User.sync({
+  force: false,
+  alter: {
+    drop: false,
+  },
+})
+  .then(() => {
+    console.log(`Modelo ${modelName} creado con éxito`);
+  })
+  .chatch((err) => {
+    console.log(`Error al crear el modelo ${modelName}:\n ${err.message}`);
+  });
+
+export default User;
