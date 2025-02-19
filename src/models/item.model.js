b47a454d229541d7781273dc0537fd9cb2fc3efa
Melany Pozo <tatianapozo585@gmail.com>
2025-02-19 16:39:40 -0500

    integracion de las ultimas propiedades a los modelos.

--- src/models/item.model.js
+++ src/models/item.model.js
@@ -8,11 +8,123 @@ const Item = sequilize.define(modelName, {
         type: DataTypes.STRING,
         allowNull: false,
     },
-    Descripcion: {
+    Description: {
         type: DataTypes.TEXT,
     },
     price: {
         type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    token: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    itemName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    variationName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    reportingCategory: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    SquareOnlineItemVisibility: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    itemType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    weight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    shippingEnabled: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    selfServerOrderingEnabled: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    deliveryEnabled: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    pickupEnabled: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    onlineSalePrice: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    archived: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    skipDetailInPOS: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    SEOTitle: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    SEODescrption: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    permalink: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    GTIN: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    socialMediaLinkTitle: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    socialMediaLinkDescrption: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    sellable: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    stockable: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    optionNAme1: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    optionValue1: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    CurrentQuantityElSaborAmbatenoIVUC: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    NewQuantytyElSaborAmbatenoIVUC: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    StocklAlertEnabledElSaborAmbatenoIVUS: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    StockAlertCountElSaborAmbatenoIVUC: {
+        type: DataTypes.STRING,
         allowNull: false,
     }
 });
