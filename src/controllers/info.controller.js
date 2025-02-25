fcff57362dcef03921894454e03a0a487a6f0bb8
Melany Pozo <tatianapozo585@gmail.com>
2025-02-25 14:26:18 -0500

    Corecciones a modulo Info

--- src/controllers/info.controller.js
+++ src/controllers/info.controller.js
@@ -10,12 +10,14 @@ export const getInfo = async () => {
 export const updateInfo = async(body) => {
     let info = await Info.findOne();
     let data;
-    let menu_qr = convertToQr(body.menu_url);
+    let menu_qr = await convertToQr(body.menu_url);
+    console.log("############################################################################");
+    console.log(menu_qr);
     body.menu_qr = menu_qr;
     if ( info ) {
-            data = Info.update(body, {where: {id: info.id}});
+            data = await Info.update(body, {where: {id: info.id}});
     }else {
-        data = Info.create(body)
+        data = await Info.create(body)
     }
     return data;
 };
